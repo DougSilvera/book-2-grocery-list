98a2cfb780a418601874854b1f313de20f5392d5
DougSilvera <shortxjdoug@gmail.com>
2021-11-18 16:44:35 -0500

    initial commit

---
+++ list.js
@@ -0,0 +1,41 @@
+const shoppingList= [
+    {
+        id: 1,
+        item: "Milk",
+        price: 3.99,
+    },
+    {
+        id: 2,
+        item: "Eggs",
+        price: 4.99,
+
+    },
+    {
+        id: 3,
+        item: "Pizza Rolls",
+        price: 8.99
+    },
+    {
+        id: 4,
+        item: "Whole Chicken",
+        price: 12.99
+    },
+    {
+        id:5,
+        item: "Bananas",
+        price: 4.99  
+    },
+    {
+        id: 6,
+        item: "sandwich buns",
+        price: 4.99
+    },
+    {
+        id: 7,
+        item: "ketchup",
+        price:5.99
+    }
+
+]
+
+console.log(shoppingList)
